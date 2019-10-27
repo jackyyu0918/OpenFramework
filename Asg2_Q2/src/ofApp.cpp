933d56adb08cabaf9b4ff3908683d78ff13ec6f4
Jacky YU <jackyyu.github@gmail.com>
2019-10-27 19:24:32 +0800

    Q2 temp

--- Asg2_Q2/src/ofApp.cpp
+++ Asg2_Q2/src/ofApp.cpp
@@ -1,16 +1,16 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+	gui.setup();
+	gui.add(lowThreshold.setup("low threshold", 50, 0, 100));
 	myVideoGrabber.initGrabber(320, 240);
-    gui.setup();
-    gui.add(lowThreshold.setup("low threshold", 50, 0, 100));
-    
-    
+
+
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
 	// Ask the grabber to refresh its data.
 	myVideoGrabber.update();
 
@@ -19,84 +19,87 @@ void ofApp::update(){
 
 		// Obtain a pointer to the grabber's image data.
 		imgCam.setFromPixels(myVideoGrabber.getPixels());
-		matRealCam = toCv(imgCam);
 		matCam = toCv(imgCam);
+		matRealCam = toCv(imgCam);
 		cvtColor(matCam, matCam, CV_BGR2GRAY);
-
 		GaussianBlur(matCam, matCam, 3);
 		Canny(matCam, matCam, lowThreshold, lowThreshold * 2);
+		//HoughCircles(matCam, circles, CV_HOUGH_GRADIENT, 2, 50, lowThreshold * 2, 100, 30, 50);
+		HoughCircles(matCam, circles, CV_HOUGH_GRADIENT, 2, 50, lowThreshold * 2, 100, 30, 50);
 	}
-}
-
-//--------------------------------------------------------------
-void ofApp::draw(){
-    ofBackground(255);
-    ofSetColor(255, 255, 255);
-	drawMat(matRealCam,0,0);
-    
-    vector<Vec3f> circles;
-    HoughCircles(matCam, circles, CV_HOUGH_GRADIENT, 2, 50, lowThreshold*2, 100, 30, 50);
-    for(int i = 0; i < circles.size(); i++){
-        ofSetColor(255, 0, 0);
-        ofNoFill();
-        ofDrawCircle(circles[i][0], circles[i][1], circles[i][2]);
-    }
-
-    gui.draw();
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::draw() {
+	ofBackground(255, 255, 255);
+	ofSetColor(255);
+	drawMat(matRealCam, 0, 0);
+	drawMat(matCam, imgCam.getWidth(), 0);
+	for (int i = 0; i < circles.size(); i++) {
+		ofSetColor(255, 255, 255);
+		ofDrawCircle(circles[i][0], circles[i][1], circles[i][2]);
+	}
+
+	for (int i = 0; i < circles.size()/2; i++) {
+		ofSetColor(0, 50, 0);
+		ofDrawCircle(circles[i][0], circles[i][1], circles[i][2]);
+	}
+	gui.draw();
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
